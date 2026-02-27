2079f9bd6915c3cfb791f653fd6960a21e60636e
emmaclaraso <emmcla575@gmail.com>
2026-02-27 11:07:32 +0000

    Exercise 14.2a

--- src/MPAGSCipher/VigenereCipher.cpp
+++ src/MPAGSCipher/VigenereCipher.cpp
@@ -15,6 +15,34 @@ setKey(key);
 }
 
 void VigenereCipher::setKey(const std::string& key){
+    // Store the key
+    key_ = key;
+    // Make sure the key is uppercase
+    std::transform(std::begin(key_), std::end(key_), std::begin(key_),
+                   ::toupper);
+    // Remove non-alphabet characters
+    key_.erase(std::remove_if(std::begin(key_), std::end(key_),
+                              [](char c) { return !std::isalpha(c); }),
+               std::end(key_));
+    // Check if the key is empty and replace with default if so
+    if (key_.empty()){
+        key_ = "DEFAULT";
+    }
+
+    // loop over the key
+
+    for (const char letter : key_) {
+        // Find the letter position in the alphabet
+        const std::size_t index{Alphabet::alphabet.find(letter)};
+
+        // Create a CaesarCipher using this position as a key
+        CaesarCipher cipher(index);
+
+        // Insert a std::pair of the letter and CaesarCipher into the lookup
+        charLookup_.insert(std::make_pair(letter, cipher));
+        }
+        
+
 }
 
 std::string VigenereCipher::applyCipher(const std::string& inputText, const CipherMode) const
